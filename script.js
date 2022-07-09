2211eb95594cc76aa1e0c268ca3fa7e7608c82aa
boompeak <wjfekr1@naver.com>
2022-07-09 21:55:00 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function back(pay) {
+    console.log(`50000 X `+ parseInt(pay/50000))
+    pay %= 50000
+    console.log(`10000 X `+ parseInt(pay/10000))
+    pay %= 10000
+    console.log(`5000 X `+ parseInt(pay/5000))
+    pay %= 5000
+    console.log(`1000 X `+ parseInt(pay/1000))
+    pay %= 1000
+    console.log(`500 X `+ parseInt(pay/500))
+    pay %= 500
+    console.log(`100 X `+ parseInt(pay/100))
+    pay %= 100
+}
+
+back(12300)
