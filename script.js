bc92eb203bb89dc18430e0db03e3b38bb9bc7582
boompeak <wjfekr1@naver.com>
2022-07-09 22:13:32 +0900

    second

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function back(pay) {
-    console.log(`50000 X `+ parseInt(pay/50000))
-    pay %= 50000
-    console.log(`10000 X `+ parseInt(pay/10000))
-    pay %= 10000
-    console.log(`5000 X `+ parseInt(pay/5000))
-    pay %= 5000
-    console.log(`1000 X `+ parseInt(pay/1000))
-    pay %= 1000
-    console.log(`500 X `+ parseInt(pay/500))
-    pay %= 500
-    console.log(`100 X `+ parseInt(pay/100))
-    pay %= 100
+  console.log(`50000 X ` + parseInt(pay / 50000));
+  pay %= 50000;
+  console.log(`10000 X ` + parseInt(pay / 10000));
+  pay %= 10000;
+  console.log(`5000 X ` + parseInt(pay / 5000));
+  pay %= 5000;
+  console.log(`1000 X ` + parseInt(pay / 1000));
+  pay %= 1000;
+  console.log(`500 X ` + parseInt(pay / 500));
+  pay %= 500;
 }
 
-back(12300)
+back(12300);
